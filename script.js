581f535b863bd83bf8585aa32bcab1fffa1693fc
nadiraputeriaulia <nadiraputeriaulia@gmail.com>
2025-08-23 22:34:32 +0700

    design page 5

---
+++ script.js
@@ -0,0 +1,31 @@
+function hitungEmisi() {
+  let aktivitas = document.getElementById("aktivitas").value;
+  let hasilDiv = document.getElementById("hasil");
+
+  if (aktivitas === "" || aktivitas < 0) {
+    hasilDiv.innerText = "Masukkan jumlah yang valid.";
+    hasilDiv.style.opacity = 1;
+    hasilDiv.style.animation = "fadeIn 0.5s ease";
+    return;
+  }
+
+  let faktorTerendah = 2.9;
+  let faktorTertinggi = 6.0;
+
+  let emisiMin = aktivitas * faktorTerendah;
+  let emisiMax = aktivitas * faktorTertinggi;
+
+  hasilDiv.innerText = `Emisi Karbon: ${emisiMin.toFixed(2)} - ${emisiMax.toFixed(2)} kg CO₂`;
+
+  hasilDiv.style.opacity = 1;
+  hasilDiv.style.animation = "fadeIn 0.5s ease";
+}
+
+// efek scroll
+window.addEventListener("scroll", () => {
+  document.body.classList.add("scrolling");
+  clearTimeout(window.scrollTimeout);
+  window.scrollTimeout = setTimeout(() => {
+    document.body.classList.remove("scrolling");
+  }, 150);
+});
